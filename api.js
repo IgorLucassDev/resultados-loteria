24e96f6cecff36404402427624605cdc6bbc74b7
IgorLucasDev <62673756+IgorLucassDev@users.noreply.github.com>
2023-10-21 23:58:54 -0300

    Clean code update

--- api.js
+++ api.js
@@ -2,12 +2,12 @@ function apiLoteria(){
     fetch('https://loteriascaixa-api.herokuapp.com/api/megasena/latest')
      .then((res) => res.json())
      .then((data) => {
-        let n1 = document.querySelector('#n1').innerHTML = data.dezenasOrdemSorteio[0]
-        let n2 = document.querySelector('#n2').innerHTML = data.dezenasOrdemSorteio[1]
-        let n3 = document.querySelector('#n3').innerHTML = data.dezenasOrdemSorteio[2]
-        let n4 = document.querySelector('#n4').innerHTML = data.dezenasOrdemSorteio[3]
-        let n5 = document.querySelector('#n5').innerHTML = data.dezenasOrdemSorteio[4]
-        let n6 = document.querySelector('#n6').innerHTML = data.dezenasOrdemSorteio[5]
+
+        for(i = 0; i < 6; i++){
+            let id = 'n' + i
+            let n = document.querySelector('#'+id)
+            n.innerHTML = data.dezenasOrdemSorteio[i]
+        }
     })
 }
 
